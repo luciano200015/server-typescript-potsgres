f2451f3460fb7fd5b3c7e152588d3fa060a96ae7
luciano <luciano@gmail.com>
2023-10-11 16:57:24 -0400

    se añadio la carpeta public

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,10 @@ import { obtenerUsuariosRoute,registerUserRoute } from './routes';
 const app=express();
 app.use(express.json())
 
+app.use(express.static('public'))
+
 app.use(obtenerUsuariosRoute);
 app.use(registerUserRoute);
-
-
 
 
 app.listen(3000,()=>{
