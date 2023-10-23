f213ae4236f52005a1869de4a67cd682b702148c
luciano200015 <lucianosorucoaltamirano02@gmail.com>
2023-10-22 22:07:21 -0400

    create base64 plato

--- src/routes/Plato/create-plato.routes.ts
+++ src/routes/Plato/create-plato.routes.ts
@@ -4,28 +4,35 @@ import validarToken from '../validarToken';
 import CreatePlatoController from '../../controllers/plato/create-plato';
 import multer from 'multer';
 import path from 'path';
+import fs from 'fs';
+
 
 const createPlatoRoute = express.Router();
 createPlatoRoute.use(validarToken);
 const ruta = path.resolve();
 
-const storage = multer.diskStorage({
+/*const storage = multer.diskStorage({
   destination: path.join(path.resolve(), 'public/imagenes-platos'),
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}-${file.originalname.trim()}`);
   }
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({ storage: storage });*/
 
-createPlatoRoute.post('/createplato',upload.single('fotoplato'), async (req, res, next) => {
-  if (!req.file) {
-    return res.status(400).json({ message: 'No se subió ningún archivo',result:null });
-  }
-  const { Nombre, Descripcion, IdUsuario, IdTipoPlato, Estado} = req.body;
-  const uploadedFile = req.file.filename;
+createPlatoRoute.post('/createplato', async (req, res, next) => {
+  const { Nombre, Descripcion, IdUsuario, IdTipoPlato, Estado, Foto } = req.body;
+  let imageName ='null';
   try {
-    const newPlato = new Plato(Nombre, Descripcion,parseInt(IdUsuario) , parseInt(IdTipoPlato),parseInt(Estado), uploadedFile);
+    if (Foto.mimeType !== null || Foto.base64 !== null) {
+      const imageBuffer = Buffer.from(Foto.base64, 'base64');
+      imageName = `${Date.now()}${Foto.mimeType}`;
+      const imagePath = path.join(ruta, 'public/imagenes-platos', imageName);
+      //console.log('imagePath: ' + imagePath);
+      // Guardar la imagen en la carpeta
+      fs.writeFileSync(imagePath, imageBuffer);
+    }
+    const newPlato = new Plato(Nombre, Descripcion, parseInt(IdUsuario), parseInt(IdTipoPlato), parseInt(Estado), imageName);
     const result = await CreatePlatoController.createPlato(newPlato);
     res.status(201).json({ results: result, message: 'Plato creado exitosamente' });
   } catch (error) {
