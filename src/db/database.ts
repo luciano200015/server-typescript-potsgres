a7d33594c8542a3a016f7c5177d05fd7fa39369a
camdevsc <camdevsc@outlook.com>
2023-11-27 15:38:03 -0400

    conection url raywall postgress

--- src/db/database.ts
+++ src/db/database.ts
@@ -1,9 +1,10 @@
 import { Pool } from "pg";
 
 export const pool=new Pool({
-    user:'postgres',
-    host:'localhost',
-    password:'1010',
+    connectionString:'postgresql://postgres:1A3eb32GfFFDE3A2Dedfad116B5*5d-c@roundhouse.proxy.rlwy.net:19791/railway'
+    /*user:'postgres',
+    host:'roundhouse.proxy.rlwy.net',
+    password:'1A3eb32GfFFDE3A2Dedfad116B5*5d-c',
     database:'RestauranteUdi',
-    port:5432
+    port:19791*/
 });
